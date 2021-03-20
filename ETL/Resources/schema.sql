a31844a5d9c41e09148eee3deea6658df99155c8
maxwellhowell8 <72665159+maxwellhowell8@users.noreply.github.com>
2021-03-20 09:02:29 -0400

    Add files via upload

---
+++ ETL/Resources/schema.sql
@@ -0,0 +1,11 @@
+-- Create Two Tables
+Create table netflix 
+(id INT primary key,
+    Netflix_Title Varchar (50),
+	year INT
+) ;
+
+Create table prime 
+(id INT primary key,
+  Prime_Title Varchar (50),
+  year INT
